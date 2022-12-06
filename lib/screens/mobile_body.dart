d160af56a52a2a2a911308634536930cbbee53ea
Srijon Mallick <srijonmallick.2000@gmail.com>
2022-12-06 19:22:11 +0530

    updated

--- lib/screens/mobile_body.dart
+++ lib/screens/mobile_body.dart
@@ -20,7 +20,6 @@ import 'package:veta/screens/user_notification.dart';
 import 'package:veta/screens/doctor_notification.dart';
 import 'package:veta/screens/notification_loader.dart';
 
-
 class MobileScaffold extends StatefulWidget {
   const MobileScaffold({Key? key}) : super(key: key);
 
@@ -32,10 +31,81 @@ class _MobileScaffoldState extends State<MobileScaffold> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       backgroundColor: defaultBackgroundColor,
-      appBar: myAppBar,
-      drawer: myDrawer,
+      appBar: AppBar(
+        backgroundColor: appBarColor,
+        title: Text('WELCOME ${user!.email!}'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (BuildContext context) {
+                return NotificationLoader(
+                  userNotification: UserNotification(),
+                  doctorNotification: DoctorNotification(),
+                );
+              }));
+            },
+            icon: const Icon(Icons.notifications),
+          )
+        ],
+        centerTitle: false,
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[300],
+        child: Column(children: [
+          DrawerHeader(
+            child: ImageIcon(AssetImage('assets/images/logo.png'), size: 160),
+          ),
+          //child: ImageIcon(AssetImage('assets/images/logo.png'), size: 160)),
+          Padding(
+            padding: tilePadding,
+            child: ListTile(
+              leading: Icon(Icons.home),
+              title: Text(
+                'D A S H B O A R D',
+                style: drawerTextColor,
+              ),
+              onTap: (() {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (BuildContext context) {
+                  return const MobileScaffold();
+                }));
+              }),
+            ),
+          ),
+          Padding(
+            padding: tilePadding,
+            child: ListTile(
+              leading: Icon(Icons.account_box),
+              title: Text(
+                'M Y  A P P O I N T M E N T S',
+                style: drawerTextColor,
+              ),
+              onTap: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (BuildContext context) {
+                  return UserAppointment();
+                }));
+              },
+            ),
+          ),
+          Padding(
+            padding: tilePadding,
+            child: ListTile(
+              leading: Icon(Icons.logout),
+              title: Text(
+                'L O G O U T',
+                style: drawerTextColor,
+              ),
+              onTap: () async {
+                await FirebaseAuth.instance.signOut();
+                //Phoenix.rebirth(context);
+              },
+            ),
+          )
+        ]),
+      ),
       body: GridView.count(
         padding: EdgeInsets.only(top: 20),
         mainAxisSpacing: 50,
@@ -45,7 +115,13 @@ class _MobileScaffoldState extends State<MobileScaffold> {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
             child: InkWell(
-              onTap: () {},
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const BookAppointment()),
+                );
+              },
               child: Ink(
                 decoration: BoxDecoration(
                   color: Colors.grey[900],
@@ -134,7 +210,6 @@ class _MobileScaffoldState extends State<MobileScaffold> {
                             color: Colors.white54,
                             fontSize: 10,
                           ),
-
                         ),
                       ],
                     ),
@@ -146,7 +221,12 @@ class _MobileScaffoldState extends State<MobileScaffold> {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
             child: InkWell(
-              onTap: () {},
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DoctorDetails()),
+                );
+              },
               child: Ink(
                 decoration: BoxDecoration(
                   color: Colors.grey[900],
